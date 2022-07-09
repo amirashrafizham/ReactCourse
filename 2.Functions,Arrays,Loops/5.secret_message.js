a5403bcfb73e5942c702696ef396ec3f60bf5295
Amir Ashraf Izham <amirashrafizham@Amir-Ashrafs-Mac.local>
2022-07-09 19:20:11 +0800

    arrays exercise

--- 2.Functions,Arrays,Loops/5.secret_message.js
+++ 2.Functions,Arrays,Loops/5.secret_message.js
@@ -26,5 +26,29 @@ let secretMessage = [
   "JavaScript",
 ];
 
+console.log(`Orignal length: ${secretMessage.length}`);
+
+// Remove last item
 secretMessage.pop();
-console.log(secretMessage);
+
+// Checking the length of the array
+console.log(secretMessage.length);
+
+// Replacing element in the array
+let easilyIndex = secretMessage.indexOf("easily");
+secretMessage[easilyIndex] = "right";
+
+// Removing the first element in the array
+secretMessage.shift();
+
+// Adding an element in the beginning of the array
+secretMessage.unshift("Programming");
+
+// Removing the sentence "get right the first time" and replace with "know"
+let index_get = secretMessage.indexOf("get");
+let index_time = secretMessage.indexOf("time");
+secretMessage.splice(index_get, index_time - index_get, "know");
+
+// Joining all elements into 1 string sentence
+let sentence = secretMessage.join(" ");
+console.log(sentence);
